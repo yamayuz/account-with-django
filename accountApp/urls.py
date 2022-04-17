86a93295242d6f73f56e720d5025f86007a70af8
yamayuz <yuki@yamayulab.com>
2022-04-17 15:38:38 +0900

    [add] signin page

--- accountApp/urls.py
+++ accountApp/urls.py
@@ -1,6 +1,7 @@
 from django.urls import path
-from .views import UserHomeView
+from .views import UserHomeView, SigninView
 
 urlpatterns = [
     path('', UserHomeView.as_view(), name='user-home'),
+    path('signin', SigninView.as_view(), name='signin'),
 ]
--- accountApp/views.py
+++ accountApp/views.py
@@ -5,3 +5,8 @@ from django.views.generic import View
 class UserHomeView(View):
     def get(self, request):
         return render(request, 'user-home.html')
+
+
+class SigninView(View):
+    def get(self, request):
+        return render(request, 'signin.html')
