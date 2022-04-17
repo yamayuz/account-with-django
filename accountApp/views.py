130ba7a0f97c6f7e8fe1a69779fa26567e2d089b
yamayuz <yuki@yamayulab.com>
2022-04-17 16:15:55 +0900

    [update] create user

--- accountApp/views.py
+++ accountApp/views.py
@@ -1,5 +1,6 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
 from django.views.generic import View
+from django.contrib.auth.models import User
 
 
 class UserHomeView(View):
@@ -15,3 +16,13 @@ class SigninView(View):
 class SignupView(View):
     def get(self, request):
         return render(request, 'signup.html')
+
+    def post(self, request):
+        target_username = request.POST['username']
+        target_password = request.POST['password']
+        try:
+            User.objects.get(username=target_username)
+            return render(request, 'signup.html', {'error': 'このユーザーは既に登録されています。'})
+        except User.DoesNotExist:
+            user = User.objects.create_user(target_username, '', target_password)
+            return redirect('signin')
